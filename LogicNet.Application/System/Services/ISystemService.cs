90c716e1df6d6e8c76ba03cf9470313608cab692
屈立亭 <543539501@qq.com>
2025-05-04 18:37:22 +0800

    利用Furion的脚手架创建项目； 从0开始；

---
+++ LogicNet.Application/System/Services/ISystemService.cs
@@ -0,0 +1,6 @@
+﻿namespace LogicNet.Application;
+
+public interface ISystemService
+{
+    string GetDescription();
+}
---
+++ LogicNet.Web.Entry/SingleFilePublish.cs
@@ -0,0 +1,22 @@
+﻿using Furion;
+using System.Reflection;
+
+namespace LogicNet.Web.Entry;
+
+public class SingleFilePublish : ISingleFilePublish
+{
+    public Assembly[] IncludeAssemblies()
+    {
+        return Array.Empty<Assembly>();
+    }
+
+    public string[] IncludeAssemblyNames()
+    {
+        return new[]
+        {
+            "LogicNet.Application",
+            "LogicNet.Core",
+            "LogicNet.Web.Core"
+        };
+    }
+}
